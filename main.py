138127eb73fa017cc2f95eaea235378fd6a58e38
Guilhermewew <141649695+Guilhermewew@users.noreply.github.com>
2026-03-26 23:56:39 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import mercadopago
 app = FastAPI()
 
 # 2. Mostramos a ele onde fica a cozinha 
-# (Você pega esses dados no painel do Supabase > Project Settings > API)
 URL_SUPABASE = "https://sdffwrpacvtjncuqtrft.supabase.co"
 CHAVE_SUPABASE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkZmZ3cnBhY3Z0am5jdXF0cmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NjU3NTAsImV4cCI6MjA5MDE0MTc1MH0.FarfM9aF-rKZOqEfhzt6Oqar1aN3H75wkHoySMAICcg"
 banco = create_client(URL_SUPABASE, CHAVE_SUPABASE)
@@ -27,18 +26,18 @@ class NovaLoja(BaseModel):
 @app.post("/lojas")
 def criar_loja(loja: NovaLoja):
     resposta = banco.table("lojas").insert({"nome": loja.nome, "slug": loja.slug}).execute()
-    return resposta.data
+    return {"mensagem": "Loja criada com sucesso!", "dados": resposta.data}
 
-# Configure com o seu Access Token de TESTE do Mercado Pago
+# 6. Configure com o seu Access Token de TESTE do Mercado Pago
 TOKEN_MP = "TEST-7623525379052412-032620-4605c07246d1051aab714eb92804a977-331454528"
 sdk = mercadopago.SDK(TOKEN_MP)
 
-# Criamos a regra do que vem no pedido
+# 7. Criamos a regra do que vem no pedido
 class NovoPedido(BaseModel):
     titulo_produto: str
     preco: float
 
-# Rota que gera o link de pagamento
+# 8. Rota que gera o link de pagamento
 @app.post("/checkout")
 def criar_pagamento(pedido: NovoPedido):
     # Montamos o carrinho
@@ -59,17 +58,3 @@ def criar_pagamento(pedido: NovoPedido):
     
     # Entregamos o link final
     return {"link_de_pagamento": resposta_mp["response"]["init_point"]}
-    # Pedimos pro Mercado Pago criar o link
-    resposta_mp = sdk.preference().create(dados_pagamento)
-    
-    # Entregamos o link final (init_point) de volta
-    return {"link_de_pagamento": resposta_mp["response"]["init_point"]}
-
-class NovaLoja(BaseModel):
-    nome: str
-
-@app.post("/lojas")
-def criar_loja(loja: NovaLoja):
-    # Manda o nome da loja lá para o banco de dados Supabase
-    resposta = supabase.table("lojas").insert({"nome": loja.nome}).execute()
-    return {"mensagem": "Loja criada com sucesso!", "dados": resposta.data}
